9636893b88a1696e010850ad6760e324dba684b3
Belabel588 <orih1402@gmail.com>
2024-11-29 17:19:03 +0200

    first header design sketch

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -3,6 +3,30 @@ export function AppHeader() {
     <header>
       <div className="web-main-header">
 
+
+        <div className="main-header-logo">
+          <img className="main-header-logo-img"
+            src="../src/assets/styles/imgs/logos/logo-white-bcg.jpg"
+            alt="mom and a half logo" />
+        </div>
+
+
+        <div>
+          <nav className="app-header-main-nav">
+            <a className="app-header-main-link" href="#">צור קשר</a>
+            <a className="app-header-main-link" href="#">המלצות</a>
+            <a className="app-header-main-link" href="#">המפגשים</a>
+            <a className="app-header-main-link" href="#">אני מאמין</a>
+            <a className="app-header-main-link" href="#">מתמודדים</a>
+            <a className="app-header-main-link" href="#">קצת עליי</a>
+
+
+
+          </nav>
+        </div>
+
+
+
       </div>
     </header>
   )
