fc7ef2583485006e4fd38e2e08584c0572299119
Belabel588 <orih1402@gmail.com>
2024-11-30 16:13:44 +0200

    centering logo in the header and small css changes

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -5,24 +5,32 @@ export function AppHeader() {
 
 
 
+
+        <div>
+          <nav className="app-header-main-nav">
+            <a className="app-header-main-link" href="#">צור קשר</a>
+            <a className="app-header-main-link" href="#">המפגשים</a>
+            <a className="app-header-main-link" href="#">המלצות</a>
+          </nav>
+        </div>
+
+
         <div className="main-header-logo">
           <img className="main-header-logo-img"
             src="../src/assets/styles/imgs/logos/logo-pink-circle-shay.png"
             alt="mom and a half logo" />
         </div>
 
+
         <div>
           <nav className="app-header-main-nav">
-            <a className="app-header-main-link" href="#">צור קשר</a>
-            <a className="app-header-main-link" href="#">המלצות</a>
-            <a className="app-header-main-link" href="#">המפגשים</a>
-            <a className="app-header-main-link" href="#">אני מאמין</a>
             <a className="app-header-main-link" href="#">מתמודדים</a>
+            <a className="app-header-main-link" href="#">אני מאמין</a>
             <a className="app-header-main-link" href="#">קצת עליי</a>
-
-
-
           </nav>
+
+
+
         </div>
 
 
--- src/cmps/Hero.jsx
+++ src/cmps/Hero.jsx
@@ -4,11 +4,11 @@ export function Hero() {
 
 
       <div className="hero-img-div">
-        <img className="hero-img" src="../src/assets/styles/imgs/images/Hero-img-7.jpg" alt="Sarit 'Mom and a half' on the couch working and smiling" />
+        <img className="hero-img" src="../src/assets/styles/imgs/images/Hero-img-8.jpg" alt="Sarit 'Mom and a half' on the couch working and smiling" />
       </div>
 
       <div className="hero-section-text">
-        <p>שלום אני שרית ״אמא וחצי״  ליווי להורות שמחה  </p>
+        <p>מלווה הורים <br></br>  להורות כייפית ושמחה </p>
       </div>
 
     </section>
