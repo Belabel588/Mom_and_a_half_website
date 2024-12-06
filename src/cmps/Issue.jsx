a8c4008a7c9423dd9341d9ca9c5c796ffd572639
Belabel588 <orih1402@gmail.com>
2024-12-06 22:47:05 +0200

    1st finalization of Issue section and component

--- src/cmps/Issue.jsx
+++ src/cmps/Issue.jsx
@@ -2,8 +2,19 @@ export function Issue() {
   return (
     <section id="issue-section">
 
+      <div className="issue-section-textbox">
+        <p className="issue-section-text">זוכרים את עצמכם דקה לפני שהפכתם להורים בפעם הראשונה? זוכרים איך היה לכם ברור איזה הורים אתם רוצים להיות? גם אתם מרגישים שהרבה פעמים אתם לא שם? קורה בדיוק ההפך?</p>
+        <h3 className="issue-section-textbox-header" >הבנתי שאנחנו יוצאים למסע לא נודע שיש בו הרבה שאלות ומעט תשובות
+        </h3>
+      </div>
+
+
+      <div className="issue-section-textbox-2">
+        <h3 className="issue-section-text-2">וזה ממש אפשרי שברוב הזמן נרגיש שאנחנו עושים עבודה די טובה<br /><br />כשהרגע שבו הבנתי שאני אמא טובה הוא גם הרגע  שבו הבנתי שאני אמא שטועה </h3>
+      </div>
+
       <div>
-        <h3 className="issue-section-header">? עם מה הורים מתמודדים </h3>
+        <h2 className="issue-section-header">?עם מה הורים מתמודדים </h2>
       </div>
 
       <div className="issue-section-img">
