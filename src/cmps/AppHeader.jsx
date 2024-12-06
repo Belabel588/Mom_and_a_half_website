a2ea9b0baa56d47eb2073c7ea4ea47e350cb5da3
Belabel588 <orih1402@gmail.com>
2024-12-06 23:33:07 +0200

    fix again paths for deployment

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -31,7 +31,7 @@ export function AppHeader() {
 
         <div className="main-header-logo">
           <img className="main-header-logo-img"
-            src="../public/logo-pink-circle-shay.png"
+            src="../public/images/logo-pink-circle-shay.png"
             alt="mom and a half logo" />
         </div>
 
