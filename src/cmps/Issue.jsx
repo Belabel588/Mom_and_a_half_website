aad0485b070d31b593927091916d3424b6b227a5
Belabel588 <orih1402@gmail.com>
2025-01-14 12:14:21 +0200

    make ipad version and pc/laptop/monitor media queries

--- src/cmps/Issue.jsx
+++ src/cmps/Issue.jsx
@@ -2,47 +2,29 @@ export function Issue() {
   return (
     <section id="issue-section">
 
-
-      {/* <div className="issue-section-start">
-        <h2 className="issue-section-header">עם מה הורים מתמודדים?</h2>
-        <h3 className="issue-section-intro-header">משפטים שאני שומעת מהורים כל הזמן:</h3>
-
-        <ul className="issue-section-list">
-          <li className="issue-section-list-item">אני מותש.ת</li>
-          <li className="issue-section-list-item">הוא לא מקשיב לי</li>
-          <li className="issue-section-list-item">אין לה חברים</li>
-          <li className="issue-section-list-item">הוא נצמד אליי ולא מוכן ללכת לאף אחד</li>
-          <li className="issue-section-list-item">כשהיא משטחת בקניון או ברחוב אין דרך להרגיע אותה</li>
-          <li className="issue-section-list-item">היא לא נרדמת עד השעות הקטנות של הלילה</li>
-          <li className="issue-section-list-item">היא לא מפסיקה להציק לאחותה</li>
-          <li className="issue-section-list-item">הוא לא מספר לי כלום</li>
-          <li className="issue-section-list-item">היא מכה אותי</li>
-          <li className="issue-section-list-item">הוא כל הזמן חוטף צעצועים</li>
-          <li className="issue-section-list-item">קשה לה להיפרד בבוקר</li>
-          <li className="issue-section-list-item">הוא מקנא באחותו הקטנה</li>
-          <li className="issue-section-list-item">היא בוכה על כל דבר</li>
-        </ul> */}
-
-
-
-      <div className="issue-section-textbox">
-        <h3 className="issue-section-textbox-header" >הרגע שבו הבנתי שאני אמא טובה הוא גם הרגע  שבו הבנתי שאני אמא שטועה
-        </h3>
-        <p className="issue-section-text">תמיד שאפתי לשלמות בכל הנוגע להורות, לעשות הכל נכון, למנוע מהילדים שלי תסכולים במאה אחוז מהזמן, להיות מודל מושלם לחיקוי עבורם.
-          <br /> לקח לי זמן להבין שהורות אינה חפה מטעויות פשוט כי אנחנו בני אדם ובמהלך הדרך סביר להניח שגם נטעה, אבל ככל שנקדים לקבל כלים נכונים לתקשורת טובה עם הילדים שלנו וליצירת אוירה משפחתית נעימה ותומכת, תחושת המסוגלות תגדל ואיתה גם התחושה שאנחנו מתקרבים למודל ההורים שאנחנו רוצים להיות.
-        </p>
-      </div>
-
-
-      <div className="issue-section-textbox-2">
-        <h3 className="issue-section-text-2"> בעצם זה ממש אפשרי שברוב הזמן נרגיש שאנחנו עושים עבודה די טובה</h3>
-      </div>
-      {/* </div> */}
-
       <div className="issue-section-img">
         <img className="issue-img" src="./images/issue-section-img-1.jpg" alt="Sarit 'Mom and a half' reading book with a smile" />
 
       </div>
+
+
+      <div className="issue-section-texts">
+
+        <div className="issue-section-textbox">
+          <h3 className="issue-section-textbox-header" >הרגע שבו הבנתי שאני אמא טובה הוא גם הרגע  שבו הבנתי שאני אמא שטועה
+          </h3>
+          <p className="issue-section-text">תמיד שאפתי לשלמות בכל הנוגע להורות, לעשות הכל נכון, למנוע מהילדים שלי תסכולים במאה אחוז מהזמן, להיות מודל מושלם לחיקוי עבורם.
+            <br /> לקח לי זמן להבין שהורות אינה חפה מטעויות פשוט כי אנחנו בני אדם ובמהלך הדרך סביר להניח שגם נטעה, אבל ככל שנקדים לקבל כלים נכונים לתקשורת טובה עם הילדים שלנו וליצירת אוירה משפחתית נעימה ותומכת, תחושת המסוגלות תגדל ואיתה גם התחושה שאנחנו מתקרבים למודל ההורים שאנחנו רוצים להיות.
+          </p>
+        </div>
+
+
+        <div className="issue-section-textbox-2">
+          <h3 className="issue-section-text-2"> בעצם זה ממש אפשרי שברוב הזמן נרגיש שאנחנו עושים עבודה די טובה</h3>
+        </div>
+      </div>
+
+
 
     </section>
   )
