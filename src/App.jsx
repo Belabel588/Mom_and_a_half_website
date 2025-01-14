f02878a1ae8aac49762596d0cc0c8f651be9b795
Belabel588 <orih1402@gmail.com>
2025-01-14 15:28:58 +0200

    mobile media query

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,10 @@ export function App() {
 
   return (
 
-    <Router>
-      <section className='app-main-layout'>
+
+
+    <section className='app-main-layout'>
+      <Router>
         <AppHeader />
         <Hero />
         {/* <a className="app-header-main-link" href="#issue-section" onClick={handleScroll}>מתמודדים</a> */}
@@ -39,10 +41,53 @@ export function App() {
         <Issue />
 
         <a href="#web-main-header">
-          <img className={`logo-nav ${showNavLogo ? 'show' : ''}`} src="./images/nav-logo-pink.png" alt="logo navigation button" />
+          <img className={`logo-nav ${showNavLogo ? 'show' : ''}`} src="./images/nav-logo-pink.png" alt=" לוגו ניווט לתחילת הדף" />
         </a>
-      </section>
-    </Router>
+      </Router>
+
+
+      {/* <div className='mobile-nav'>
+
+        <div className='mobile-nav-link-container'>
+          <img className='mobile-nav-logo' src="./images/nav-logo-pink.png" alt="לוגו ניווט לצור קשר" />
+          <a className="app-header-main-link" href="#">צור קשר</a>
+        </div>
+
+        <div className='mobile-nav-link-container'>
+          <img className='mobile-nav-logo' src="./images/nav-logo-pink.png" alt="לוגו ניווט לאני מאמין" />
+          <a className="app-header-main-link" href="#">אני מאמין</a>
+
+        </div>
+        <div className='mobile-nav-link-container'>
+          <img className='mobile-nav-logo' src="./images/nav-logo-pink.png" alt="לוגו ניווט לקצת עליי" />
+          <a className="app-header-main-link" href="#">קצת עליי</a>
+        </div>
+
+        <div className='mobile-nav-link-container'>
+          <img className='mobile-nav-logo' src="./images/nav-logo-pink.png" alt="לוגו ניווט למפגשים" />
+          <a className="app-header-main-link" href="#">המפגשים</a>
+        </div>
+
+
+
+        <div className='mobile-nav-link-container'>
+          <img className='mobile-nav-logo' src="./images/nav-logo-pink.png" alt="לוגו ניווט להמלצות" />
+          <a className="app-header-main-link" href="#">המלצות</a>
+        </div>
+
+
+        <div className='mobile-nav-link-container'>
+          <img className='mobile-nav-logo' src="./images/nav-logo-pink.png" alt="לוגו ניווט למתמודדים" />
+          <a className="app-header-main-link" href="#issue-section-start">מתמודדים</a>
+        </div>
+
+
+
+      </div> */}
+
+
+    </section >
+
   )
 }
 
